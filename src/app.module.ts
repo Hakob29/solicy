e69df40de51bff19228a31aca637cb9425f9c34e
Hakob29 <hakobyan29001@gmail.com>
2023-08-02 20:33:04 +0400

    User Asset Catalog and Product modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from 'ormconfig';
+import { CatalogModule } from './catalog/catalog.module';
+import { UserModule } from './user/user.module';
+import { AssetModule } from './asset/asset.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(AppDataSource.options)
+    TypeOrmModule.forRoot(AppDataSource.options),
+    CatalogModule,
+    UserModule,
+    AssetModule,
+    ProductModule
   ],
   controllers: [],
   providers: [],
---
+++ src/catalog/catalog.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CatalogController } from './catalog.controller';
+import { CatalogService } from './catalog.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature()],
+  controllers: [CatalogController],
+  providers: [CatalogService]
+})
+export class CatalogModule { }
